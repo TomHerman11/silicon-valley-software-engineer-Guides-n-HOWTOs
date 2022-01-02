05e881526e5cb7d2e01279f6f85bd2ba53ee778e
Tom Herman <TomHerman11@gmail.com>
2022-01-01 23:19:18 -0800

    upload Variables content

--- Kotlin/src/main/kotlin/Variables.kt
+++ Kotlin/src/main/kotlin/Variables.kt
@@ -1,2 +1,25 @@
-class Variables {
+val h = 8
+
+fun main() {
+    // Variables in Kotlin
+    val a = 1
+    val b: Int
+    b = 1
+
+    var c = 3
+    c = 33
+    var d: Int
+    d = 4
+    d = 44
+
+    // Types
+    val e: Int = 5
+    val f: String = "f"
+    val g = 7 // Int
+
 }
+
+// TIP - val (when possible) + Inferred Type
+val tom = "happy"
+
+// Like & subscribe!
